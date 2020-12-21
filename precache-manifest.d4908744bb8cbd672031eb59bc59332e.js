b803d9158df75f5b6c0cbc167ce16694e933cb7d
Madhusudhana RK <madhusudhana.rk@care.com>
2020-12-21 06:58:53 +0530

    Updates

---
+++ precache-manifest.d4908744bb8cbd672031eb59bc59332e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b07103b6f8765bfed5fc26c68082901",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ac3ff307e8ea665c79f",
+    "url": "./static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1bfc2334644c93305063",
+    "url": "./static/css/main.461ed6cf.chunk.css"
+  },
+  {
+    "revision": "6ac3ff307e8ea665c79f",
+    "url": "./static/js/2.64ef0836.chunk.js"
+  },
+  {
+    "revision": "cb1e829ba5c2861d037fef2ca75f476f",
+    "url": "./static/js/2.64ef0836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bfc2334644c93305063",
+    "url": "./static/js/main.cca29243.chunk.js"
+  },
+  {
+    "revision": "64bf4b176c310251f90f",
+    "url": "./static/js/runtime-main.69e6e381.js"
+  },
+  {
+    "revision": "0b823006c32aaea6c91be5583357e256",
+    "url": "./static/media/NoData.0b823006.png"
+  }
+]);
